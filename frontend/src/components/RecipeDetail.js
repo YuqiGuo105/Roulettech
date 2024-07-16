a6c2514571b4760f602c2cea8b4c222ba72598d8
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2024-07-16 14:55:51 -0400

    WYSIWYG Content Update
    Bug fix for loading WYSIWYG Content

--- frontend/src/components/RecipeDetail.js
+++ frontend/src/components/RecipeDetail.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import DOMPurify from 'dompurify';
 
 const RecipeDetail = () => {
   const { id } = useParams();
@@ -24,8 +25,15 @@ const RecipeDetail = () => {
     <div className="relative md:col-span-1">
       <h1 className="text-2xl font-bold mb-4">{recipe.name}</h1>
       <div className="relative mb-4">
-        <p className="text-justify whitespace-pre-line">{recipe.content}</p>
-        <img className="absolute top-0 left-1/2 transform -translate-x-1/2 w-6/12 h-2/5 ml-4  object-cover rounded" src={recipe.imageURL} alt={recipe.name} />
+        <div
+          className="text-justify"
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(recipe.content) }}
+        ></div>
+        <img
+          className="absolute top-0 left-1/2 transform -translate-x-1/2 w-6/12 h-2/5 ml-4 object-cover rounded"
+          src={recipe.imageURL}
+          alt={recipe.name}
+        />
       </div>
     </div>
   );
